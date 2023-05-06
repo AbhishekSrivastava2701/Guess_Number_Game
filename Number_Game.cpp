f218164ec8ea9541cee561e4d0d504952c81b521
Abhishek Srivastava <113105622+AbhishekSrivastava2701@users.noreply.github.com>
2023-05-06 22:49:42 +0530

    A Funny game of guessing number.
    You can import the #include <cstdlib>
    #include <ctime>
    so that you can use random numbers in your code.

---
+++ Number_Game.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    int randomNumber, guess;
+    srand(time(NULL));
+    randomNumber = rand() % 100 + 1;
+    cout << "Guess the number between 1 and 100: ";
+    cin >> guess;
+
+    while (guess != randomNumber) {
+        if (guess < randomNumber) {
+            cout << "Too low. Guess again: ";
+        } else {
+            cout << "Too high. Guess again: ";
+        }
+        cin >> guess;
+    }
+
+    cout << "Congratulations! You guessed the number.\n";
+    return 0;
+}
